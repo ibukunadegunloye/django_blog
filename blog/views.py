bd59ef5c3a6a012cc8f08b62e764f1f4b4cddd17
shaovafest <ibkife1@gmail.com>
2022-11-16 19:09:06 +0100

    template inheritance

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
 
+posts = [
+    {
+        'author': 'Ibukun',
+        'title': 'Blog Post 1',
+        'content':'First post content',
+        'date_posted': 'November 16, 2022'
+    },
+    {
+        'author': 'Yinka',
+        'title': 'Blog Post 2',
+        'content':'Second post content',
+        'date_posted': 'November 17, 2022'
+    }
+]
+
 def home(request):
-    return(HttpResponse('<h1>Blog Home</h1>'))
+    context = {
+        'posts': posts
+    }
+    return(render(request, 'blog/home.html', context))
 
 
 def about(request):
-    return(HttpResponse('<h1>Blog About</h1>'))
+    return(render(request, 'blog/about.html', {'title': 'About'}))
