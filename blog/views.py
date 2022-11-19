fe1efff79ace3daffc0bc4b06d6d981956185f26
shaovafest <ibkife1@gmail.com>
2022-11-19 02:39:38 +0100

    migrations

--- blog/views.py
+++ blog/views.py
@@ -1,32 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 
 
 # Create your views here.
 
-posts = [
-    {
-        'author': 'Ibukun',
-        'title': 'Blog Post 1',
-        'content':'First post content',
-        'date_posted': 'November 16, 2022'
-    },
-    {
-        'author': 'Yinka',
-        'title': 'Blog Post 2',
-        'content':'Second post content',
-        'date_posted': 'November 17, 2022'
-    },
-    {
-        'author': 'Goke',
-        'title': 'Blog Post 3',
-        'content':'Third post content',
-        'date_posted': 'November 18, 2022'
-    }
-]
-
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return(render(request, 'blog/home.html', context))
 
