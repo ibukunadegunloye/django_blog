9d20e8689c53188588920c2b5151128d2b9b0abe
shaovafest <ibkife1@gmail.com>
2022-12-05 09:16:27 +0100

    reset email with google smtp, already confirm it works but i need to delete my app password as i didnt create any abstractions while creating this feature, going out now, but will use an os environ to do this and re commit when i get back. but yeah tested a few times and it works so im pushing this!

--- django_project/settings.py
+++ django_project/settings.py
@@ -135,3 +135,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'blog-home'
 
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+DEFAULT_FROM_EMAIL = 'default from email'
